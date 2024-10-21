6bca8ee20384268694d7d3a31a39e2d357837380
nainika0305 <nainika3103@gmail.com>
2024-10-21 15:27:32 +0530

    Firebase configure

---
+++ lib/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_watch/authenticate.dart';
+//import 'package:wallet_watch/hi.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //either return login or dashboard
+    return const Auth();
+  }
+}
