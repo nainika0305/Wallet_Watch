95df24aeb814613561bb943de52edcef2a25ea3c
nainika0305 <nainika3103@gmail.com>
2024-10-26 02:19:52 +0530

    Working sign in and sign out buttons - basic ui

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,13 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:wallet_watch/auth/authenticate.dart';
-//import 'package:wallet_watch/hi.dart';
+import 'package:wallet_watch/homepage.dart';
+import 'package:wallet_watch/login_page.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //either return login or dashboard
-    return const Auth();
+    return Scaffold(
+        body: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Homepage();
+              }
+              else {
+                return LoginPage();
+              }
+            }
+        )
+    );
   }
 }
