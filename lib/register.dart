8592d8b70541ac0478ed195773f244866a405ed7
nainika0305 <nainika3103@gmail.com>
2024-12-19 19:22:36 +0530

    final ui of register

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class RegisterPage extends StatefulWidget {
   final VoidCallback showLoginPage;
@@ -14,18 +14,15 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  // Controllers
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmpasswordController = TextEditingController();
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
 
-  // Password Feedback Message and Style
   String _passwordFeedbackMessage = '';
   Color _passwordFeedbackColor = Colors.red;
 
-  // Dropdown-related variables
   final List<String> currencies = ['USD', 'EUR', 'INR', 'JPY', 'GBP'];
   String? selectedCurrency;
 
@@ -39,35 +36,24 @@ class _RegisterPageState extends State<RegisterPage> {
     super.dispose();
   }
 
-  // Sign-Up Logic
   Future signUp() async {
     if (_passwordConfirmed()) {
       try {
-        // Firebase Auth User Creation
         await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: _emailController.text.trim(),
           password: _passwordController.text.trim(),
         );
-
-        // Add User Details to Firestore
         await addUserDetails(
           _firstNameController.text.trim(),
           _lastNameController.text.trim(),
           _emailController.text.trim(),
           selectedCurrency ?? 'Not Selected',
         );
-
-        // Navigate to Login or Next Page
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Registration Successful')),
-
         );
-
-
-
       } on FirebaseAuthException catch (e) {
-        // Display appropriate error messages
-        String message = 'An error occurred';
+        String message = 'Please fill all fields';
         if (e.code == 'email-already-in-use') {
           message = 'Email already in use. Please use a different email.';
         } else if (e.code == 'invalid-email') {
@@ -109,11 +95,6 @@ class _RegisterPageState extends State<RegisterPage> {
         _passwordFeedbackMessage =
         'Password must include uppercase, lowercase, a number, and a special character.';
         _passwordFeedbackColor = Colors.red;
-      } else if (password.contains(_emailController.text.trim()) ||
-          password.contains(_firstNameController.text.trim()) ||
-          password.contains(_lastNameController.text.trim())) {
-        _passwordFeedbackMessage = 'Password should not contain personal details.';
-        _passwordFeedbackColor = Colors.red;
       } else {
         _passwordFeedbackMessage = 'Password looks good!';
         _passwordFeedbackColor = Colors.green;
@@ -121,7 +102,6 @@ class _RegisterPageState extends State<RegisterPage> {
     });
   }
 
-  // Custom TextField Builder
   Widget _buildTextField(
       TextEditingController controller,
       String hintText, {
@@ -130,14 +110,22 @@ class _RegisterPageState extends State<RegisterPage> {
       }) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        onChanged: onChanged,
-        decoration: InputDecoration(
-          hintText: hintText,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFFDA4BA).withOpacity(0.35),
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: TextField(
+            controller: controller,
+            obscureText: obscureText,
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: hintText,
+            ),
           ),
         ),
       ),
@@ -147,143 +135,154 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: const Color(0xFFBCB6DE),
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 50),
-                // Logo
-                const Icon(
-                  Icons.android,
-                  size: 100,
-                ),
-                const SizedBox(height: 25),
-                // Title
-                const Text(
-                  'Hello There!',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Register below with your details!',
-                  style: TextStyle(fontSize: 20),
-                ),
-                const SizedBox(height: 25),
-                // First Name Field
-                _buildTextField(_firstNameController, 'First Name'),
-                const SizedBox(height: 15),
-                // Last Name Field
-                _buildTextField(_lastNameController, 'Last Name'),
-                const SizedBox(height: 15),
-                // Email Field
-                _buildTextField(_emailController, 'Email'),
-                const SizedBox(height: 15),
-                // Currency Dropdown
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: DropdownButtonFormField<String>(
-                    value: selectedCurrency,
-                    hint: const Text('Select Currency'),
-                    items: currencies
-                        .map((currency) => DropdownMenuItem(
-                      value: currency,
-                      child: Text(currency),
-                    ))
-                        .toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        selectedCurrency = value;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFFFDBE9),
+                Color(0xFFE6D8FF),
+                Color(0xFFBDE0FE),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 70),
+                  Transform.translate(
+                    offset: Offset(0, -80),
+                    child: Image.asset(
+                      'assests/Untitled_design-removebg-preview.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                  Transform.translate(
+                    offset: Offset(0, -60),
+                    child: const Text(
+                      'Register Here!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Color(0xFF003366),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 15),
-                // Password Field with Feedback
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildTextField(
-                      _passwordController,
-                      'Password (min 6 chars)',
-                      obscureText: true,
-                      onChanged: checkPasswordStrength,
-                    ),
-                    const SizedBox(height: 8),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                      child: Text(
-                        _passwordFeedbackMessage,
-                        style: TextStyle(
-                          color: _passwordFeedbackColor,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w500,
+                  Transform.translate(
+                    offset: Offset(0, -30),
+                    child: _buildTextField(_firstNameController, 'First Name'),
+                  ),
+
+                  Transform.translate(
+                    offset: Offset(0, -15),
+                    child:  _buildTextField(_lastNameController, 'Last Name'),
+                  ),
+
+                  _buildTextField(_emailController, 'Email'),
+                  const SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: DropdownButtonFormField<String>(
+                      value: selectedCurrency,
+                      hint: const Text('Select Currency'),
+                      items: currencies
+                          .map((currency) => DropdownMenuItem(
+                        value: currency,
+                        child: Text(currency),
+                      ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCurrency = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
                       ),
                     ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                // Confirm Password Field
-                _buildTextField(
-                  _confirmpasswordController,
-                  'Confirm Password',
-                  obscureText: true,
-                ),
-                const SizedBox(height: 25),
-                // Sign-Up Button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: signUp,
-                    child: Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius: BorderRadius.circular(12),
+                  ),
+                  const SizedBox(height: 15),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildTextField(
+                        _passwordController,
+                        'Password',
+                        obscureText: true,
+                        onChanged: checkPasswordStrength,
                       ),
-                      child: const Center(
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25.0),
                         child: Text(
-                          'Sign Up',
+                          _passwordFeedbackMessage,
                           style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                            color: _passwordFeedbackColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  _buildTextField(
+                    _confirmpasswordController,
+                    'Confirm Password',
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: GestureDetector(
+                      onTap: signUp,
+                      child: Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF73A5C6),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 15),
-                // Redirect to Login
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'I am a member!',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    GestureDetector(
-                      onTap: widget.showLoginPage,
-                      child: const Text(
-                        ' Login now',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Already a member?  ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      GestureDetector(
+                        onTap: widget.showLoginPage,
+                        child: const Text(
+                          'Login now',
+                          style: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
